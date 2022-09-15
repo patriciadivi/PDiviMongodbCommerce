a8866aaccf167efbadce994ccd8e8b20d5c71e86
PatriciaDivi <patriciavianad@gmail.com>
2022-09-15 01:36:14 -0300

    refactor: :truck: Correção na contagem na chave 'totalProdutos'

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,5 +1,8 @@
 db.resumoProdutos.insertOne(
-  { franquia: "McDonalds", totalProdutos: 5 },
+  { 
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.find().count(),
+  },
 );
 db.resumoProdutos.find(
   {}, { _id: 0, franquia: 1, totalProdutos: 1 },
